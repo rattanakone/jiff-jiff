b588ea2cffb1057c5349ee61c4b71d77f4342265
rattanakone <rattanakone@users.noreply.github.com>
2015-11-03 22:34:05 -0800

    angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jiffJiffApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the jiffJiffApp
+ */
+angular.module('jiffJiffApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jiffJiffApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the jiffJiffApp
+ */
+angular.module('jiffJiffApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
